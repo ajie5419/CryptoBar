765bd27af885da5f00360aec14491be6dbcc1d1f
ajie5419 <ajie5419@gmail.com>
2025-06-26 14:55:30 +0800

    Initial Commit

---
+++ CryptoBar/CryptoBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoBarApp.swift
+//  CryptoBar
+//
+//  Created by 赵小杰 on 2025/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CryptoBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoBarTests/CryptoBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoBarTests.swift
+//  CryptoBarTests
+//
+//  Created by 赵小杰 on 2025/6/26.
+//
+
+import Testing
+@testable import CryptoBar
+
+struct CryptoBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
